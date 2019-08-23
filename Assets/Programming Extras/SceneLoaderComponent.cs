e811aafd46e12140a5b906f571961f5541d8f52c
Riley Baker <vishnukey@gmail.com>
2019-08-22 22:34:00 -0600

    setup base project with a handful of useful extras

---
+++ Assets/Programming Extras/SceneLoaderComponent.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoaderComponent : MonoBehaviour
+{
+    [SerializeField] private string sceneToLoadName;
+    [SerializeField] private bool additive;
+
+    private void OnEnable()
+    {
+        LoadSceneMode loadSceneMode = additive ? LoadSceneMode.Additive : LoadSceneMode.Single;
+        SceneManager.LoadScene(sceneToLoadName, loadSceneMode);
+    }
+}
