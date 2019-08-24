327e150188915e1b3c963c754f18c8345b35d8de
Riley Baker <vishnukey@gmail.com>
2019-08-24 09:32:06 -0600

    Made general collision event trigger

---
+++ Assets/Scripts/ColliderTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ColliderTrigger : MonoBehaviour
+{
+    [SerializeField] UnityEvent m_event;
+
+    private void Awake()
+    {
+        if (m_event == null) m_event = new UnityEvent();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            m_event.Invoke();
+    }
+}
