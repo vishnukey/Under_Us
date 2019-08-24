6fbec6647a4898d84a08c4a5d5ad79a30fb3ae03
Phantom1st <caelum.is.amazing@gmail.com>
2019-08-23 21:26:47 -0600

    Added basic screenspace shader to write fog overtop of the skybox.

---
+++ Assets/Fog/PostProcessBlit.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[ExecuteAlways]
+public class PostProcessBlit : MonoBehaviour
+{
+    [SerializeField] private Material materialToRenderWith;
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (!materialToRenderWith) return;
+        Graphics.Blit(source, destination, materialToRenderWith);
+    }
+}
