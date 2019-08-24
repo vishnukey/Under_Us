1eebaf963d9c9810d28e254c1c343c8be27f16c4
Daniel Doran <ddoran3@gmail.com>
2019-08-23 23:10:06 -0600

    camera control

---
+++ Assets/Scripts/CameraLook.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLook : MonoBehaviour {
+	// Start is called before the first frame update
+	float rotationY = 0.0f;
+	float rotationX = 0.0f;
+	void Start() {
+
+	}
+
+	// Update is called once per frame
+	void Update() {
+		rotationX += Input.GetAxis("Mouse X") * Time.deltaTime;
+		rotationY += Input.GetAxis("Mouse Y") * Time.deltaTime;
+		rotationY = Mathf.Clamp(rotationY, -89.9f, 89.9f);
+		transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+	}
+}
