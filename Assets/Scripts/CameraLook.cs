fc0b61125b3000d7c4b38c7fa319b112b037c47c
Daniel Doran <ddoran3@gmail.com>
2019-08-24 00:02:59 -0600

    'tab' enables mouse lock

--- Assets/Scripts/CameraLook.cs
+++ Assets/Scripts/CameraLook.cs
@@ -6,12 +6,22 @@ public class CameraLook : MonoBehaviour {
 	public float sensitivity = 60f;
 	float rotationY = 0.0f;
 	float rotationX = 0.0f;
+
+	bool mouseLock = false;
 	void Start() {
 
 	}
 
 	// Update is called once per frame
 	void Update() {
+		if (Input.GetKeyDown(KeyCode.Tab)) mouseLock = !mouseLock;
+
+		if (mouseLock) {
+			Cursor.lockState = CursorLockMode.Locked;
+		} else {
+			Cursor.lockState = CursorLockMode.None;
+		}
+
 		rotationX += Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
 		rotationY += Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
 		rotationY = Mathf.Clamp(rotationY, -89.9f, 89.9f);
