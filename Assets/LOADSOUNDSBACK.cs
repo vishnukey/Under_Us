29187cf1d7e3424fff5ca2973907e7fabc395953
Phantom1st <caelum.is.amazing@gmail.com>
2019-08-25 00:39:37 -0600

    SpookyWords

--- Assets/LOADSOUNDSBACK.cs
+++ Assets/LOADSOUNDSBACK.cs
@@ -10,29 +10,30 @@ public class LOADSOUNDSBACK : MonoBehaviour
     [SerializeField] private GameObject fab;
     [SerializeField] private Transform underMe;
 
+    [SerializeField] private float stretch;
 
-
+    [ContextMenu("go")]
     void Start()
     {
-        int i = 0;
-        foreach (var clip in clips)
-        {
-            var thing = Instantiate(fab, (-Vector3.up * i), Quaternion.Euler(Vector3.zero));
-            thing.name = clip.name;
-            thing.transform.GetChild(0).GetComponent<AudioSource>().clip = clip;
-            i++;
-
-            thing.transform.SetParent(underMe);
-        }
-
         //int i = 0;
-
-        //Transform child = transform.GetChild(i);
-        //while (child != null)
+        //foreach (var clip in clips)
         //{
-        //    child = transform.GetChild(i);
-        //    child.localPosition = -Vector3.up * i;
+        //    var thing = Instantiate(fab, (-Vector3.up * i), Quaternion.Euler(Vector3.zero));
+        //    thing.name = clip.name;
+        //    thing.transform.GetChild(0).GetComponent<AudioSource>().clip = clip;
         //    i++;
+
+        //    thing.transform.SetParent(underMe);
         //}
+
+        int i = 0;
+
+        Transform child = transform.GetChild(i);
+        while (child != null)
+        {
+            child = transform.GetChild(i);
+            child.localPosition = -Vector3.up * i * stretch;
+            i++;
+        }
     }
 }
